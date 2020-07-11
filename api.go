ed1c58164ce30101b98e5eea964a11789a92ac46
Ariel Simulevski <ariel@simulevski.at>
2020-07-11 17:47:50 +0200

    Add some documentation

--- api.go
+++ api.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Azer0s/quacktors/util"
 )
 
+// Self returns the PID of the caller goroutine/actor
 func Self() pid.Pid {
 	goid := util.GetGoid()
 	p, err := actors.GetByGoid(goid)
@@ -18,19 +19,23 @@ func Self() pid.Pid {
 	return p
 }
 
+// Spawn spawns an actor by a function and returns the actors PID
 func Spawn(action func()) pid.Pid {
 	return actors.Spawn(action)
 }
 
+// Send sends data to a PID, this is a non-blocking call
 func Send(pid pid.Pid, data interface{}) {
-	pid.Send(data)
+	go pid.Send(data)
 }
 
+// Receive receives data sent to the caller goroutine/actor, this is a blocking call
 func Receive() interface{} {
 	p := Self()
 	return util.PidToLocalPid(p).Receive()
 }
 
+// Monitor monitors a PID, when the state of the monitored PID goes down, a message is sent to the monitoring actor
 func Monitor(toMonitor pid.Pid) {
 	p := Self()
 
