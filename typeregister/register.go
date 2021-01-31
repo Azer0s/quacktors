f55ed5eb05ca92409363a8c60970968b0823fb65
Ariel Simulevski <ariel@simulevski.at>
2021-01-31 19:28:58 +0100

    Add doc

--- typeregister/register.go
+++ typeregister/register.go
@@ -4,10 +4,12 @@ import "sync"
 
 var typeRegistry = &sync.Map{}
 
+//Store stores a type by name.
 func Store(name string, value interface{}) {
 	typeRegistry.Store(name, value)
 }
 
+//Load loads a type by name.
 func Load(name string) (interface{}, bool) {
 	return typeRegistry.Load(name)
 }
